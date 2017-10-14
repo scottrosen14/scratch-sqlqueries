51695750997aea40a08636e085cfd023fd5f068e
Vince Vuong <vuong.vincent@gmail.com>
2017-10-14 12:36:24 -0700

    Added entity relationship image to ER page

--- src/components/ErPage.js
+++ src/components/ErPage.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react';
+import '../styles/ErPage.css';
+import er from '../ER.png'; 
 
 class ErPage extends Component {
   render() {
       return (
+        <div>
         <p>ER</p>
+        <img src={er} />
+        </div>
       )
   }
 }
