8903fc430af807c5d9c87d8fbbe110ef4476bb23
Jelena Gruica <jelena.gruica@gmail.com>
2017-10-13 20:20:57 -0700

    new changes

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
-let pg = require('pg');
+const pg = require('pg');
 
 // Set up the express app
 const app = express();
@@ -11,22 +11,37 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(express.static(path.resolve(__dirname, '../build')));
 
-let conString = process.env.ELEPHANTSQL_URL='postgres://iexsauup:1tWt0Au0dYbAAC63zJNqm6ZQbrUo5bs8@elmer.db.elephantsql.com:5432/iexsauup' || "postgres://postgres:5432@localhost/postgres";
+// CONNECTION TO THE ELEPHANT
+// var conString = 'postgres://iexsauup:1tWt0Au0dYbAAC63zJNqm6ZQbrUo5bs8@elmer.db.elephantsql.com:5432/iexsauup';
+// var client = new pg.Client(conString);
+// client.connect(function (err, result) {
+//     if (err) {
+//         return console.error('could not connect to postgres', err);
+//     } else {
+//         console.log('connected to the db');
+//         client.query('SELECT * FROM city;', (err, result) => {
+//             if (err) throw new Error(err);
+//             console.log("result", result.rows);
+//             client.end();
+//         });
+//     }
+// });
 
-let client = new pg.Client(conString);
-client.connect(function(err) {
-  if(err) {
-    return console.error('could not connect to postgres', err);
-  }
-  client.query('SELECT NOW() AS "theTime"', function(err, result) {
-    if(err) {
-      return console.error('error running query', err);
-    }
-    console.log(result.rows[0].theTime);
-    //output: Tue Jan 15 2013 19:12:47 GMT-600 (CST)
-    client.end();
-  });
-});
+// connection to the local db
+// steps to local db
+//
+// const uri = 'postgres://player:1234@localhost/sqlgame';
+
+// pg.connect(uri, (err, db) => {
+//   if (err) throw new Error(err);
+//   console.log("connected to db");
+// //   make SQL queries
+//   db.query('SELECT * FROM city;', (err, result) => {
+//     if (err) throw new Error(err);
+//     console.log("result", result.rows);
+//     db.end();
+//   });
+// });
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname + './../build/index.html'));
